4f23e44a191283b3d38cdcec21b00f6ee47d5de7
conk7 <super.g5433@yandex.ru>
2024-02-02 20:59:11 +0300

    added axes and grid render

--- main.cpp
+++ main.cpp
@@ -3,16 +3,76 @@
 #include <SFML/Window.hpp>
 #include <sstream>
 #include <vector>
+#include <math.h>
+
+void draw_axes(sf::RenderWindow &window, sf::View const &view, unsigned const gridSize)
+{
+    sf::Vector2f viewCenter = view.getCenter();
+    sf::Vector2f viewSize = view.getSize();
+    float viewLeftBorder = viewCenter.x - (viewSize.x / 2);
+    float gridLeftBorder = floor(viewLeftBorder / gridSize) * gridSize;
+    float viewRightBorder = viewCenter.x + (viewSize.x / 2);
+    float gridRightBorder = ceil(viewRightBorder / gridSize) * gridSize;
+    float viewTopBorder = viewCenter.y - (viewSize.y / 2);
+    float gridTopBorder = ceil(viewTopBorder / gridSize) * gridSize;
+    float viewBottomBorder = viewCenter.y + (viewSize.y / 2);
+    float gridBottomBorder = ceil(viewBottomBorder / gridSize) * gridSize;
+    
+    float gridIdx = gridLeftBorder;
+
+    while (gridIdx < gridRightBorder)
+    {
+        sf::RectangleShape line;
+        line.setFillColor(sf::Color::Black);
+        line.setPosition(gridIdx, gridTopBorder - gridSize);
+        line.setSize(sf::Vector2f(1, viewSize.y + gridSize));
+        window.draw(line);
+        gridIdx += gridSize;
+    }
+
+    gridIdx = gridBottomBorder;
+    while (gridIdx >= gridTopBorder)
+    {
+        sf::RectangleShape line;
+        line.setFillColor(sf::Color::Black);
+        line.setPosition(gridLeftBorder - gridSize, gridIdx);
+        line.setSize(sf::Vector2f(viewSize.x + gridSize * 2, 1));
+        window.draw(line);
+        gridIdx -= gridSize;
+    }
+
+    if(gridBottomBorder > 0 && gridTopBorder < 0)
+    {
+        sf::RectangleShape line;
+        line.setFillColor(sf::Color::Black);
+        line.setPosition(gridLeftBorder - gridSize, 0);
+        line.setSize(sf::Vector2f(viewSize.x + gridSize * 2, 3));
+        window.draw(line);
+        gridIdx += gridSize;
+    }
+
+    if(gridRightBorder > 0 && gridLeftBorder < 0)
+    {
+        sf::RectangleShape line;
+        line.setFillColor(sf::Color::Black);
+        line.setPosition(0, gridTopBorder - gridSize);
+        line.setSize(sf::Vector2f(3, viewSize.y + gridSize));
+        window.draw(line);
+        gridIdx += gridSize;
+    }
+
+}
 
 int main()
 {
+    //default width and height of the window
     const uint16_t SCREEN_WIDTH = 1920;
     const uint16_t SCREEN_HEIGHT = 1080;
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "52");  // sf::Style::Fullscreen
 
+    //creating and configuring the window
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "52");  // sf::Style::Fullscreen
     window.setVerticalSyncEnabled(true);
     window.setKeyRepeatEnabled(true);
-
 
     //grid
     static float gridSizeF = 50.f;
@@ -27,24 +87,15 @@ int main()
     rect_shape.setPosition(100.f,100.f);
     shape.setFillColor(sf::Color::Green);
 
+    // const int mapSizeX = window.getSize().x / gridSizeU + 3;
+    // const int mapSizeY = window.getSize().y / gridSizeU + 3;
+    // std::vector<std::vector<sf::RectangleShape>> tileMap;
+    // tileMap.resize(mapSizeX, std::vector<sf::RectangleShape>());
 
-    const int mapSize = 100;
-    std::vector<std::vector<sf::RectangleShape>> tileMap;
-    tileMap.resize(mapSize, std::vector<sf::RectangleShape>());
-
-    for (int x = 0; x < mapSize; x++)
-    {
-        tileMap[x].resize(mapSize, sf::RectangleShape());
-        for (int y = 0; y < mapSize; y++)
-        {
-            tileMap[x][y].setSize(sf::Vector2f(gridSizeF, gridSizeF));
-            tileMap[x][y].setFillColor(sf::Color::White);
-            tileMap[x][y].setOutlineThickness(2.f);
-            tileMap[x][y].setOutlineColor(sf::Color::Black);
-            tileMap[x][y].setPosition(x * gridSizeF, y * gridSizeF);
-        }
-        
-    }
+    sf::RectangleShape background;
+    background.setFillColor(sf::Color::White);
+    background.setSize(sf::Vector2f(static_cast<float>(window.getSize().x), 
+                        static_cast<float>(window.getSize().y))); //полнейшая хуетень but works
 
     int fromX = 0;
     int fromY = 0;
@@ -78,10 +129,11 @@ int main()
     text.setFillColor(sf::Color::White);
     text.setFont(font);
     text.setPosition(20.f, 20.f);
-    text.setString("TEST");
+    text.setOutlineColor(sf::Color::Black);
+    text.setOutlineThickness(1.f);
 
-    sf::Vector2f prevMousePos;
-    sf::Vector2f currMousePos;
+    sf::Vector2i prevMousePos;
+    sf::Vector2i currMousePos;
 
     while (window.isOpen())
     {
@@ -93,7 +145,7 @@ int main()
 
         //update mouse pos
         prevMousePos = currMousePos;
-        currMousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+        currMousePos = sf::Mouse::getPosition(window);
 
         mousePosScreen = sf::Mouse::getPosition();
         mousePosWindow = sf::Mouse::getPosition(window);
@@ -105,8 +157,34 @@ int main()
             mousePosGrid.y = mousePosView.y / gridSizeU;
         window.setView(window.getDefaultView());
 
+        sf::Vector2i negmousePosGrid;
+        negmousePosGrid.x = mousePosView.x / gridSizeU;
+        negmousePosGrid.y = mousePosView.y / gridSizeU;
+
         //update game elements
-        tileSelector.setPosition(mousePosGrid.x * gridSizeF, mousePosGrid.y * gridSizeF);
+        // tileSelector.setPosition(mousePosGrid.x * gridSizeF, 
+                                    // mousePosGrid.y * gridSizeF); //old tile selecton
+        if(negmousePosGrid.x >= 0 && negmousePosGrid.y >= 0)
+        {
+            tileSelector.setPosition(negmousePosGrid.x * gridSizeF, 
+                                    negmousePosGrid.y * gridSizeF); //new tile selection that works incorrectly
+        }                                                        //with negative values
+        else if(negmousePosGrid.x < 0 && negmousePosGrid.y >= 0)
+        {
+            tileSelector.setPosition(negmousePosGrid.x * gridSizeF - gridSizeF, 
+                                    negmousePosGrid.y * gridSizeF);
+        }
+        else if(negmousePosGrid.x > 0 && negmousePosGrid.y < 0)
+        {
+            tileSelector.setPosition(negmousePosGrid.x * gridSizeF, 
+                                    negmousePosGrid.y * gridSizeF - gridSizeF);
+        }
+        else if(negmousePosGrid.x < 0 && negmousePosGrid.y < 0)
+        {
+            tileSelector.setPosition(negmousePosGrid.x * gridSizeF - gridSizeF, 
+                                    negmousePosGrid.y * gridSizeF - gridSizeF);
+        }
+
 
         //update ui
         std::stringstream ss;
@@ -140,55 +218,32 @@ int main()
             {
                 view.move((prevMousePos.x - currMousePos.x), 
                             (prevMousePos.y - currMousePos.y));
-                prevMousePos = currMousePos;
+                // prevMousePos = currMousePos;
             }          
 
         }
-        //render
+
+        //render begins
         window.clear();
+
+        sf::FloatRect visibleArea(0, 0, window.getSize().x, window.getSize().y);
+        window.setView(sf::View(visibleArea));
+        window.draw(background);
         
         //render game elements
         window.setView(view);
-
-        fromX = view.getCenter().x / gridSizeF - 7;
-        toX = view.getCenter().x / gridSizeF + 7;
-        fromY = view.getCenter().y / gridSizeF  - 7;
-        toY = view.getCenter().y / gridSizeF + 7;
-
-        if(fromX < 0)
-            fromX = 0;
-        else if(fromX >= mapSize)
-            fromX = mapSize - 1;
-        if(fromY < 0)
-            fromY = 0;
-        else if(fromY >= mapSize)
-            fromY = mapSize - 1;
-
-        if(toX < 0)
-            toX = 0;
-        else if(toX >= mapSize)
-            toX = mapSize - 1;
-        if(toY < 0)
-            toY = 0;
-        else if(toY >= mapSize)
-            toY = mapSize - 1;
        
-        for (int x = fromX; x < toX; x++)
-        {
-            for (int y = fromY; y < toY; y++)
-            {
-                window.draw(tileMap[x][y]);
-            }
-        }
+        draw_axes(window, view, gridSizeU);
         
         window.draw(tileSelector);
         // window.setView(window.getDefaultView());
-        sf::FloatRect visibleArea(0, 0, window.getSize().x, window.getSize().y);
+        // sf::FloatRect visibleArea(0, 0, window.getSize().x, window.getSize().y);
         window.setView(sf::View(visibleArea));
 
         //render ui
         window.draw(text);
         window.display();
+        //render ends
     }
 
     return 0;
